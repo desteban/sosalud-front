1b7b3aa894b4601f4ea500687f54401f1e3a6cd3
desteban <esteban28c@hotmail.com>
2022-10-24 21:58:26 -0500

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="contacto">
+        <div>
+          <StaticImage src="../images/Sosalud.png" alt="Logo sosalud" />
+        </div>
+
+        <div>
+          <p>
+            Somos una sociedad que está comprometida con la gestión y operación
+            integral en los procesos de facturación y auditoria.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
